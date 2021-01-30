5621fd9c1ee871ffcf1a3cbac99401a81930b656
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2021-01-30 15:20:58 -0600

    First Commit

---
+++ VeryEasy/VEasy.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE MySQLWeek1;
+
+USE MySQLWk1;
+
+
+
+CREATE TABLE Cars (
+
+ id INT PRIMARY KEY,
+ make VARCHAR(300),
+ model VARCHAR(300),
+ model_year Year
+);
+
+SELECT * FROM Cars;
+
---
+++ VeryEasy/VeryEasy.sql
@@ -0,0 +1,4 @@
+INSERT INTO Cars (id,make, model, model_year)
+VALUES ('3','Mercedez-Benz', 'GLA', '2020');
+ -- ('Audi', 'Q8', '2020'),
+--  ('Mercedez-Benz', 'GLA', '2020')
