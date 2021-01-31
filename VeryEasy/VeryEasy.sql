7b476d7cbda0c9778f433232cb9fbc2cd01bdf1c
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2021-01-30 20:50:29 -0600

    Completed

--- VeryEasy/VeryEasy.sql
+++ VeryEasy/VeryEasy.sql
@@ -1,4 +1,19 @@
-INSERT INTO Cars (id,make, model, model_year)
-VALUES ('3','Mercedez-Benz', 'GLA', '2020');
- -- ('Audi', 'Q8', '2020'),
---  ('Mercedez-Benz', 'GLA', '2020')
+CREATE DATABASE MySQLWeek1;
+
+USE MySQLWeek1;
+
+
+CREATE TABLE Cars (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ make VARCHAR(100),
+ model VARCHAR(100),
+ model_year YEAR
+);
+
+INSERT INTO Cars (make, model, model_year)
+VALUES ('Porche', 'Cayenne', '2021'),
+	   ('Audi', 'Q8', '2020'),
+       ('Mercedez-Benz', 'GLA', '2020');
+       
+
+       
